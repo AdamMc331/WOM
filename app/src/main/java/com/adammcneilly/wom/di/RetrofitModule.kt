e4e789e17b8886c1f0b543ffa22614ea95180582
Adam McNeilly <amcneilly331@gmail.com>
2024-01-13 22:47:25 -0500

    Cleaning up retrofit module.

--- app/src/main/java/com/adammcneilly/wom/di/RetrofitModule.kt
+++ app/src/main/java/com/adammcneilly/wom/di/RetrofitModule.kt
@@ -14,29 +14,21 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 @Module
 @InstallIn(SingletonComponent::class)
 object RetrofitModule {
-    @Provides
-    fun provideMoshi(): Moshi {
-        return Moshi
-            .Builder()
-            .build()
-    }
 
     @Provides
-    fun provideHttpClient(): OkHttpClient {
+    fun provideWiseOldManAPI(): WiseOldManAPI {
+        val moshi = Moshi
+            .Builder()
+            .build()
+
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
 
-        return OkHttpClient
+        val client = OkHttpClient
             .Builder()
             .addInterceptor(loggingInterceptor)
             .build()
-    }
 
-    @Provides
-    fun provideWiseOldManAPI(
-        moshi: Moshi,
-        client: OkHttpClient,
-    ): WiseOldManAPI {
         return Retrofit
             .Builder()
             .client(client)
