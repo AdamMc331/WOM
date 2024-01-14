2384e4193a5a8a8781696c1b90389186b628bb34
Adam McNeilly <amcneilly331@gmail.com>
2024-01-14 12:39:07 -0500

    Tweaking sizing and preview.

--- app/src/main/java/com/adammcneilly/wom/ui/components/SkillCard.kt
+++ app/src/main/java/com/adammcneilly/wom/ui/components/SkillCard.kt
@@ -1,12 +1,17 @@
 package com.adammcneilly.wom.ui.components
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.adammcneilly.wom.debug.debugSkillList
 import com.adammcneilly.wom.models.Skill
 import com.adammcneilly.wom.theme.WOMTheme
@@ -20,14 +25,23 @@ fun SkillCard(
     skills: List<SkillDisplayModel>,
     modifier: Modifier = Modifier,
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(SKILL_COLUMNS),
+    Card(
         modifier = modifier,
     ) {
-        items(skills) { skill ->
-            SkillIconLevel(
-                displayModel = skill,
-            )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(SKILL_COLUMNS),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+            items(skills) { skill ->
+                SkillIconLevel(
+                    displayModel = skill,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .wrapContentSize(),
+                )
+            }
         }
     }
 }
@@ -45,6 +59,10 @@ private fun SkillCardPreview() {
     val skills = debugSkillList().map(Skill::toDisplayModel)
 
     WOMTheme {
-        SkillCard(skills = skills)
+        SkillCard(
+            skills = skills,
+            modifier = Modifier
+                .padding(16.dp),
+        )
     }
 }
